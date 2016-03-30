ef52e84fd69f7e311013d50e5712fcf22e482e71
xavarr <xavier.arroues@aramisauto.com>
2016-03-30 09:41:38 +0200

    Integration Zendesk API

--- src/CoreBundle/Controller/DefaultController.php
+++ src/CoreBundle/Controller/DefaultController.php
@@ -6,86 +6,86 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class DefaultController extends Controller
 {
-    /**
-     * @param string $companyPhone
-     * @param string $companyInternalPhone
-     * @param string $companyPrenom
-     * @param string $companyEmail
-     * @param string $companyNom
-     * @param string $companyPassword
-     * @param string $companyAgence
-     * @param string $companyService
-     * @param string $companyExtension
-     * @return array
-     */
-    private function createWithTemplateArrayForRequestFromCompany($companyPhone, $companyInternalPhone, $companyPrenom, $companyEmail, $companyNom, $companyPassword, $companyAgence, $companyService, $companyExtension)
-    {
-        return $userBeanWithTemplateInfos = array(
-            'active' => "1",
-            'data' => $companyPhone,
-            'ddiNumber' => $companyInternalPhone,
-            'directAccessCode' => $companyPhone,
-            'firstName' => $companyPrenom,
-            'language' => "1",
-            'listAgentGroup' => NULL,
-            'listTemplate' => $companyAgence."_".$companyService,
-            'mail' => $companyEmail,
-            'name' => $companyNom,
-            'overloadGroup' => "true",
-            'overloadTemplate' => "true",
-            'password' => $companyPassword,
-            'phoneLoginNumber' => $companyPhone,
-            'phoneLoginPassword' => NULL,
-            'templateId' => $companyAgence."_".$companyService,
-            'timeZone' => "fr",
-            'userId' => $companyExtension,
-            'skillId' => NULL,
-            'skillLevel' => NULL
-        );
-    }
-
-    /**
-     * @param string $companyInternalPhone
-     * @param string $companyPrenom
-     * @param string $companyEmail
-     * @param string $companyNom
-     * @param string $companyPassword
-     * @param string $companyAgence
-     * @param string $companyExtension
-     * @return array
-     */
-    private function createArrayForRequestFromCompany($companyInternalPhone, $companyPrenom, $companyEmail, $companyNom, $companyPassword, $companyAgence, $companyExtension)
-    {
-        return $userBeanInfos = array(
-            'activeState' => "1",
-            'agentGroups' => "ga_".strtoupper($companyAgence),
-            'breaks' => NULL,
-            'calendarId' => "GENERAL",
-            'ddiNumber' => $companyInternalPhone,
-            'disruptionDuration' => "15",
-            'email' => $companyEmail,
-            'enable2WaysSwitch' => "true",
-            'enableCallBack' => "true",
-            'enableConference' => "true",
-            'enableOutCall' => "true",
-            'enableRecording' => "true",
-            'firstName' => $companyPrenom,
-            'languageId' => "1",
-            'loginTel' => $companyInternalPhone,
-            'name' => $companyNom,
-            'password' => $companyPassword,
-            'transfertDDIAuthorized' => "true",
-            'userId' => $companyExtension,
-            'wrapupTime' => "15",
-            'level' => "3",
-            'skillKeyword' => "MARKET",
-            'specialty' => "1",
-        );
-    }
-
-    /**
-     * @return \Symfony\Component\HttpFoundation\Response
-     */
+//    /**
+//     * @param string $companyPhone
+//     * @param string $companyInternalPhone
+//     * @param string $companyPrenom
+//     * @param string $companyEmail
+//     * @param string $companyNom
+//     * @param string $companyPassword
+//     * @param string $companyAgence
+//     * @param string $companyService
+//     * @param string $companyExtension
+//     * @return array
+//     */
+//    private function createWithTemplateArrayForRequestFromCompany($companyPhone, $companyInternalPhone, $companyPrenom, $companyEmail, $companyNom, $companyPassword, $companyAgence, $companyService, $companyExtension)
+//    {
+//        return $userBeanWithTemplateInfos = array(
+//            'active' => "1",
+//            'data' => $companyPhone,
+//            'ddiNumber' => $companyInternalPhone,
+//            'directAccessCode' => $companyPhone,
+//            'firstName' => $companyPrenom,
+//            'language' => "1",
+//            'listAgentGroup' => NULL,
+//            'listTemplate' => $companyAgence."_".$companyService,
+//            'mail' => $companyEmail,
+//            'name' => $companyNom,
+//            'overloadGroup' => "true",
+//            'overloadTemplate' => "true",
+//            'password' => $companyPassword,
+//            'phoneLoginNumber' => $companyPhone,
+//            'phoneLoginPassword' => NULL,
+//            'templateId' => $companyAgence."_".$companyService,
+//            'timeZone' => "fr",
+//            'userId' => $companyExtension,
+//            'skillId' => NULL,
+//            'skillLevel' => NULL
+//        );
+//    }
+//
+//    /**
+//     * @param string $companyInternalPhone
+//     * @param string $companyPrenom
+//     * @param string $companyEmail
+//     * @param string $companyNom
+//     * @param string $companyPassword
+//     * @param string $companyAgence
+//     * @param string $companyExtension
+//     * @return array
+//     */
+//    private function createArrayForRequestFromCompany($companyInternalPhone, $companyPrenom, $companyEmail, $companyNom, $companyPassword, $companyAgence, $companyExtension)
+//    {
+//        return $userBeanInfos = array(
+//            'activeState' => "1",
+//            'agentGroups' => "ga_".strtoupper($companyAgence),
+//            'breaks' => NULL,
+//            'calendarId' => "GENERAL",
+//            'ddiNumber' => $companyInternalPhone,
+//            'disruptionDuration' => "15",
+//            'email' => $companyEmail,
+//            'enable2WaysSwitch' => "true",
+//            'enableCallBack' => "true",
+//            'enableConference' => "true",
+//            'enableOutCall' => "true",
+//            'enableRecording' => "true",
+//            'firstName' => $companyPrenom,
+//            'languageId' => "1",
+//            'loginTel' => $companyInternalPhone,
+//            'name' => $companyNom,
+//            'password' => $companyPassword,
+//            'transfertDDIAuthorized' => "true",
+//            'userId' => $companyExtension,
+//            'wrapupTime' => "15",
+//            'level' => "3",
+//            'skillKeyword' => "MARKET",
+//            'specialty' => "1",
+//        );
+//    }
+//
+//    /**
+//     * @return \Symfony\Component\HttpFoundation\Response
+//     */
     public function indexAction()
     {
 //        $loadCreateAgentErrorsCodes = $this->getParameter('odigo_wsdl_error_createagent_codes');
@@ -118,31 +118,31 @@ class DefaultController extends Controller
         return $this->render('CoreBundle:Default:index.html.twig');
     }
 
-    public function deleteAction($companyExtensionId)
-    {
-        $loadDeleteErrorsCodes = $this->getParameter('odigo_wsdl_error_export_codes');
-
-        $delete = $this->get('odigo.service.client')->delete(
-            $this->getParameter('odigo'),
-            $companyExtensionId
-        );
-        echo "<br><br>";
-        echo $loadDeleteErrorsCodes[$delete];
-
-        return $this->render('CoreBundle:Default:index.html.twig');
-    }
-
-    public function exportAction()
-    {
-        $loadExportErrorsCodes = $this->getParameter('odigo_wsdl_error_delete_codes');
-
-        $export = $this->get('odigo.service.client')->export(
-            $this->getParameter('odigo'),
-            $this->getParameter('odigo_login')
-        );
-        echo "<br><br>";
-        echo $loadExportErrorsCodes[$export];
-
-        return $this->render('CoreBundle:Default:index.html.twig');
-    }
+//    public function deleteAction($companyExtensionId)
+//    {
+//        $loadDeleteErrorsCodes = $this->getParameter('odigo_wsdl_error_export_codes');
+//
+//        $delete = $this->get('odigo.service.client')->delete(
+//            $this->getParameter('odigo'),
+//            $companyExtensionId
+//        );
+//        echo "<br><br>";
+//        echo $loadDeleteErrorsCodes[$delete];
+//
+//        return $this->render('CoreBundle:Default:index.html.twig');
+//    }
+//
+//    public function exportAction()
+//    {
+//        $loadExportErrorsCodes = $this->getParameter('odigo_wsdl_error_delete_codes');
+//
+//        $export = $this->get('odigo.service.client')->export(
+//            $this->getParameter('odigo'),
+//            $this->getParameter('odigo_login')
+//        );
+//        echo "<br><br>";
+//        echo $loadExportErrorsCodes[$export];
+//
+//        return $this->render('CoreBundle:Default:index.html.twig');
+//    }
 }

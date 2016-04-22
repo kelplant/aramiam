09e4fd1a4f7c2aa62f06c6335876e7c9c6c99c12
xavarr <xavier.arroues@aramisauto.com>
2016-04-22 22:54:31 +0200

    javascript correction

--- web/js/iam.js
+++ web/js/iam.js
@@ -309,12 +309,10 @@ function generatePassword(formName)
     var randomstring = '';
     var charCount = 0;
     var numCount = 0;
-
     for (var i=0; i<string_length; i++) {
         // If random bit is 0, there are less than 3 digits already saved, and there are not already 5 characters saved, generate a numeric value.
         if((Math.floor(Math.random() * 2) == 0) && numCount < 3 || charCount >= 5) {
             var rnum = Math.floor(Math.random() * 10);
-
             randomstring += rnum;
             numCount += 1;
         } else {

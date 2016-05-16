bb2814db3f00b1e33468c85d7cd66c486cb36e9f
xavarr <xavier.arroues@aramisauto.com>
2016-05-16 02:36:52 +0200

    random protrait

--- src/DashboardBundle/Controller/DashboardController.php
+++ src/DashboardBundle/Controller/DashboardController.php
@@ -82,7 +82,7 @@ class DashboardController extends Controller
             $this->ifToday($startDate);
             $this->ifHier($startDate);
             $this->ifNotTodayOrHier($startDate);
-            $finalTab[] = array('viewName' => $member->getViewName(), 'id' => $member->getId(), 'startDate' => $this->startDate);
+            $finalTab[] = array('viewName' => $member->getViewName(), 'id' => $member->getId(), 'startDate' => $startDate, 'civilite' => $member->getCivilite());
         }
 
         $finalTab = array('countNewUser' => $countNewUser, 'finalTab' => $finalTab);
@@ -118,35 +118,6 @@ class DashboardController extends Controller
             $finalTodoListEvents[] = array('delais' => $delais, 'color' => $this->gimmeColorForDelais($delais), 'id' => $event->getId(), 'name' => $event->getName(), 'comment' => $event->getComment(), 'createDate' => date('Y-m-d', strtotime($event->getCreateDate()->format('Y-m-d'))), 'isDone' => $event->getIsDone());
         }
         return $finalTodoListEvents;
-    }
-
-    /**
-     * @Route("/", name="homepage_dashboard")
-     */
-    public function indexAction()
-    {
-        $session_messaging = $this->get('session')->get('messaging');
-        $this->get('session')->set('messaging', []);
-        $globalAlertColor = $this->get('core.index.controller_service')->getGlobalAlertColor($session_messaging);
-
-        $candidatListe  = $this->get('core.candidat_manager')->getRepository()->findBy(array('isArchived' => '0'), array('startDate' => 'ASC'));
-        $lastest_users  = $this->lastest_users();
-        $todoListEvents = $this->prepareTodoListEvents();
-
-        return $this->render('DashboardBundle:Default:dashboard.html.twig', array(
-            'entity'                        => '',
-            'nb_candidat'                   => count($candidatListe),
-            'candidat_color'                => $this->get('core.index.controller_service')->colorForCandidatSlider($candidatListe[0]->getStartDate()->format("Y-m-d")),
-            'session_messaging'             => $session_messaging,
-            'currentUserInfos'              => $this->get('security.token_storage')->getToken()->getUser(),
-            'userPhoto'                     => $this->get('google.google_user_api_service')->base64safeToBase64(stream_get_contents($this->get('security.token_storage')->getToken()->getUser()->getPhoto())),
-            'lastest_members'               => $lastest_users['finalTab'],
-            'countNewUser'                  => $lastest_users['countNewUser'],
-            'todoListEvents'                => $todoListEvents,
-            'globalAlertColor'              => $globalAlertColor,
-            'remaining_gmail_licenses'      => $this->get('app.parameters_calls')->getParam('remaining_google_licenses'),
-            'remaining_salesforce_licenses' => $this->get('app.parameters_calls')->getParam('remaining_licences_type_Salesforce'),
-        ));
     }
 
     /**
@@ -221,6 +192,35 @@ class DashboardController extends Controller
         $finalTab['last'] = $last;
 
         return $finalTab;
+    }
+
+    /**
+     * @Route("/", name="homepage_dashboard")
+     */
+    public function indexAction()
+    {
+        $session_messaging = $this->get('session')->get('messaging');
+        $this->get('session')->set('messaging', []);
+        $globalAlertColor = $this->get('core.index.controller_service')->getGlobalAlertColor($session_messaging);
+
+        $candidatListe  = $this->get('core.candidat_manager')->getRepository()->findBy(array('isArchived' => '0'), array('startDate' => 'ASC'));
+        $lastest_users  = $this->lastest_users();
+        $todoListEvents = $this->prepareTodoListEvents();
+
+        return $this->render('DashboardBundle:Default:dashboard.html.twig', array(
+            'entity'                        => '',
+            'nb_candidat'                   => count($candidatListe),
+            'candidat_color'                => $this->get('core.index.controller_service')->colorForCandidatSlider($candidatListe[0]->getStartDate()->format("Y-m-d")),
+            'session_messaging'             => $session_messaging,
+            'currentUserInfos'              => $this->get('security.token_storage')->getToken()->getUser(),
+            'userPhoto'                     => $this->get('google.google_user_api_service')->base64safeToBase64(stream_get_contents($this->get('security.token_storage')->getToken()->getUser()->getPhoto())),
+            'lastest_members'               => $lastest_users['finalTab'],
+            'countNewUser'                  => $lastest_users['countNewUser'],
+            'todoListEvents'                => $todoListEvents,
+            'globalAlertColor'              => $globalAlertColor,
+            'remaining_gmail_licenses'      => $this->get('app.parameters_calls')->getParam('remaining_google_licenses'),
+            'remaining_salesforce_licenses' => $this->get('app.parameters_calls')->getParam('remaining_licences_type_Salesforce'),
+        ));
     }
 
     /**

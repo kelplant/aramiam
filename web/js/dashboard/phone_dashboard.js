30ffbbe3b0bd176455835ac1fb15d78e6c199fcb
xavarr <xavier.arroues@aramisauto.com>
2016-06-26 16:46:13 +0200

    defilement dashboard numéros par bouttons

--- web/js/dashboard/phone_dashboard.js
+++ web/js/dashboard/phone_dashboard.js
@@ -2,10 +2,10 @@
 
     var scrollHandle = 0,
         scrollStep = 5,
-        parent = $("#container");
+        parent = $("#containerOrange");
 
     //Start the scrolling process
-    $(".panner").on("mouseenter", function () {
+    $(".pannerOrange").on("mouseenter", function () {
         var data = $(this).data('scrollModifier'),
             direction = parseInt(data, 10);
 
@@ -15,7 +15,47 @@
     });
 
     //Kill the scrolling
-    $(".panner").on("mouseleave", function () {
+    $(".pannerOrange").on("mouseleave", function () {
+        stopScrolling();
+        $(this).removeClass('active');
+    });
+
+    //Actual handling of the scrolling
+    function startScrolling(modifier, step) {
+        if (scrollHandle === 0) {
+            scrollHandle = setInterval(function () {
+                var newOffset = parent.scrollLeft() + (scrollStep * modifier);
+
+                parent.scrollLeft(newOffset);
+            }, 10);
+        }
+    }
+
+    function stopScrolling() {
+        clearInterval(scrollHandle);
+        scrollHandle = 0;
+    }
+
+}());
+
+(function () {
+
+    var scrollHandle = 0,
+        scrollStep = 5,
+        parent = $("#containerProsodie");
+
+    //Start the scrolling process
+    $(".pannerProsodie").on("mouseenter", function () {
+        var data = $(this).data('scrollModifier'),
+            direction = parseInt(data, 10);
+
+        $(this).addClass('active');
+
+        startScrolling(direction, scrollStep);
+    });
+
+    //Kill the scrolling
+    $(".pannerProsodie").on("mouseleave", function () {
         stopScrolling();
         $(this).removeClass('active');
     });
